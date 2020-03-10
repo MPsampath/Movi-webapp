aca8ffbbbb2d7cccea8f97b0f78f2b493df703f6
MPsampath <prasannasampath199651@gmail.com>
2020-03-11 02:05:16 +0530

    creat navbar and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <div className="d-flex flex-column bg-dark">
+  <footer className="footer">
+    <div>
+      <a href="https://coreui.io">CoreUI</a>
+      <span>&copy; 2020 creativeLabs.</span>
+    </div>
+    <div className="ml-auto">
+      <span>Powered by</span>
+      <a href="https://coreui.io">CoreUI</a>
+    </div>
+  </footer>
+</div>
+        </div>
+    )
+}
+
+export default Footer
